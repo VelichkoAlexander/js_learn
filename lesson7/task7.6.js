423b9215d28dc36698ad503c546832a20ef861e9
Alexander Velichko <alvi@mail.ua>
2016-01-18 01:53:40 +0200

    lesson7 task7.6 done

---
+++ lesson7/task7.6.js
@@ -0,0 +1,17 @@
+/**Напишите две бинарные функции add и mul которые
+ * складывают и умножают свои 2 аргумента.
+ * add(3,4); //7
+ * mul(3,4); //12
+ */
+
+
+function add(a, b) {
+	return a+b;
+}
+
+function mul(a, b) {
+	return a*b;
+
+}
+console.log(add(3,4));
+console.log(mul(3,4));
