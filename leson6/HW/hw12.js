e7b67749991a55381aa4b251ac635e37cff02685
Alexander Velichko <alvi@mail.ua>
2016-01-18 01:50:01 +0200

    lesson6 hw12 done

--- leson6/HW/hw12.js
+++ leson6/HW/hw12.js
@@ -1,3 +1,36 @@
-/**
- * Created by Alexandr on 18.01.2016.
- */
+function getVillainName(birthday) {
+	var months = ["The Evil",
+		"The Vile",
+		"The Cruel",
+		"The Trashy",
+		"The Despicable",
+		"The Embarrassing",
+		"The Disreputable",
+		"The Atrocious",
+		"The Twirling",
+		"The Orange",
+		"The Terrifying",
+		"The Awkward"];
+
+	var days = ["Mustache",
+		"Pickle",
+		"Hood Ornament",
+		"Raisin",
+		"Recycling Bin",
+		"Potato",
+		"Tomato",
+		"House Cat",
+		"Teaspoon",
+		"Laundry Basket"];
+
+	var monthStr = months[birthday.getMonth()];
+
+	var day = birthday.getDate();
+
+	var dayStr = days[day.toString().slice(-1)];
+
+	return monthStr+' '+dayStr;
+
+}
+var date = new Date(1984,10,18);
+console.log(getVillainName(date));
