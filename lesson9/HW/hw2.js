91923b5586ef46a08a38b536f84b274e59d2802b
Alexander Velichko <alvi@mail.ua>
2016-01-22 12:11:59 +0200

    lesson9  hw2 done

---
+++ lesson9/HW/hw2.js
@@ -0,0 +1,17 @@
+function applyAll(func){
+	var arr = [].slice.call(arguments,1);
+	return func.apply(null, arr)
+}
+
+
+
+
+
+
+
+
+// Применить Math.max к аргументам 2, -2, 3
+console.log( applyAll(Math.max, 2, -2, 3) ); // 3
+
+// Применить Math.min к аргументам 2, -2, 3
+console.log( applyAll(Math.min, 2, -2, 3) ); // -2
