677e680eb931346f099d82255dd2facc53aa70a4
Alexander Velichko <alvi@mail.ua>
2016-03-18 23:18:29 +0200

    lesson 15  7q.3

--- lesson15/Codewars 7q/hw3.js
+++ lesson15/Codewars 7q/hw3.js
@@ -1,3 +1,23 @@
 /**
- * Created by Alexandr on 10.03.2016.
+ * http://www.codewars.com/kata/js-prototypes-module-number-1-object-prototypes
  */
+
+
+var widget = {
+	foo: "bar"
+};
+
+var gadget = {
+	bar: "foo"
+};
+
+var thingamabob = {
+	bar: "food"
+};
+
+
+var proto = {
+	description : "TODO: GIVE ME A DESCRIPTION"
+};
+
+Object.prototype.description = "TODO: GIVE ME A DESCRIPTION";
