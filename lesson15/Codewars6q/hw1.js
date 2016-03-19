31ce61a1c1f67d499936b459e4f29a335d9342f1
Alexander Velichko <alvi@mail.ua>
2016-03-19 13:24:46 +0200

    lesson 15  6q.1  done

--- lesson15/Codewars6q/hw1.js
+++ lesson15/Codewars6q/hw1.js
@@ -1,33 +1,40 @@
 /**
  * Created by Alexandr on 18.03.2016.
  */
+
 function NamedOne(first, last) {
 // -- SHOULD be changed --
 	this.firstName = first;
 	this.lastName = last;
-	//this.fullName = this.firstName + ' ' + this.lastName;
 	Object.defineProperty(this, "fullName", {
 
-		get: function() {
+		get: function () {
 			return this.firstName + ' ' + this.lastName;
 		},
 
-		set: function(value) {
-			if(value ===)
+		set: function (value) {
 			var split = value.split(' ');
-			this.firstName = split[0];
-			this.lastName = split[1];
+			var fullName = this.firstName + ' ' + this.lastName;
+			console.log(split.length >= 2);
+			if (!(value == fullName) && split.length === 2) {
+				console.log("go");
+				this.firstName = split[0];
+				this.lastName = split[1];
+			}
 		}
 	});
 
 }
 
-var namedOne = new NamedOne("Naomi","Wang");
 
-namedOne.firstName = "John";
+var namedOne = new NamedOne("Naomi", "Wang");
+
+namedOne.firstName = "John_sdsd";
 namedOne.lastName = "Doe";
+namedOne.fullName = "Schtruwwe lPeter";
+
 // ...then...
-console.log(namedOne.fullName); // -> "John Doe"K
+console.log(namedOne.firstName); // -> "John Doe"K
 
 //// -- And :
 //namedOne.fullName = "Bill Smith";
